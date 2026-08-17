c1f5bb1e3ba47d69909998839e7accc1945c358a
ROOPAGAYATRI DEVARAPALLI <166210253+Roopa771@users.noreply.github.com>
2026-08-17 11:40:09 +0530

    Time: 3 ms (84.87%) | Memory: 21.2 MB (18.53%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,15 +1,5 @@
+from collections import Counter
+
 class Solution:
     def majorityElement(self, nums):
-        candidate = None
-        count = 0
-
-        for num in nums:
-            if count == 0:
-                candidate = num
-
-            if num == candidate:
-                count += 1
-            else:
-                count -= 1
-
-        return candidate
+        return Counter(nums).most_common(1)[0][0]
